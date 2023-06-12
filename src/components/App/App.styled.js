3c8d0e05f2e3e9e85629bd9b8acf4b0e284e880e
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-06-12 09:48:35 +0200

    feedback

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 350px;
+  border-radius: 8px;
+  padding: 10px;
+  margin: 0 auto;
+  text-align: center;
+  background-color: white;
+  background: linear-gradient(159deg, #c2bcd8, #446f8b, #4abde3);
+  box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
+    1px 4px 6px rgba(0, 0, 0, 0.16);
+  /* 
+  ); W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
---
+++ src/components/Section/Sections.styled.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.h2`
+  color: red;
+  text-shadow: 2px 2px 5px black;
+  font-size: 30px;
+`;
